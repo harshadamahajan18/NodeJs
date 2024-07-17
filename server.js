6cf42b83e5f92a69047f67cc260864299f6a20d9
harshada mahajan <harshadamahajan51@gmail.com>
2024-07-17 14:37:56 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -113,9 +113,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());  // req.body
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to my hotel.. How i can help you? We have list of menus')
@@ -128,6 +130,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 // Use the router
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
+
 
 app.listen(3000, ()=>{
   console.log('listening on port 3000')
